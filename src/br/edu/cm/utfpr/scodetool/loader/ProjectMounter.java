b3dc8656dbb53b80ce0e8493c04bb0f9ffc8851d
Paulo B da Costa <pauloc@linuxmail.org>
2016-05-11 14:56:45 -0300

    adicionado arquivo de interface ProjectMounter - referente a issue #9

---
+++ src/br/edu/cm/utfpr/scodetool/loader/ProjectMounter.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.edu.cm.utfpr.scodetool.loader;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import br.edu.cm.utfpr.scodetool.entities.generics.Project;
+import br.edu.cm.utfpr.scodetool.entities.generics.SourceCode;
+
+/**
+ * @author Paulo
+ *
+ */
+public interface ProjectMounter<S extends SourceCode, PA extends Package, PO extends Project> {
+    public ArrayList<S> mountSourceCodes(ArrayList<File> files);
+    public ArrayList<PA> mountPackages(ArrayList<S> sourceCodes);
+    public PO mountProject(ArrayList<PA> packages);
+}
